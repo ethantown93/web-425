e97cf3252c2ce102abfbbe04119f3e7b8a8a9fcc
Ethan Townsend <eetfitness@gmail.com>
2019-07-10 20:17:03 -0700

    adding week 1 assignments

---
+++ week-1/es5-typescript-example/townsend-assignment-1.4.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title:  townsend-assignment-1.4
+; Author: Ethan Townsend
+; Date:   7/10/2019
+; Description: web-425
+;===========================================
+*/
+
+var firstName = 'Ethan';
+var lastName = 'Townsend';
+function myName(firstName, lastName) {
+    return 'Hello ' + firstName + ' ' + lastName;
+}
+
+console.log(myName(firstName, lastName));
---
+++ week-1/es5-typescript-example/townsend-assignment-1.4.ts
@@ -0,0 +1,15 @@
+/*
+============================================
+; Title:  townsend-assignment-1.4
+; Author: Ethan Townsend
+; Date:   7/10/2019
+; Description: web-425
+;===========================================
+*/
+
+let firstName: string = 'Ethan';
+let lastName: string = 'Townsend';
+
+function myName(firstName: string, lastName: string) {
+    return 'Hello ' + firstName + ' ' + lastName;
+}
