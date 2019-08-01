b97c9c8a5cc9ca56a00ec2fb34132879b7f294fa
ethan townsend <eetfitness@gmail.com>
2019-07-31 18:16:59 -0700

    adding week 4 assignments

---
+++ week-4/form-control/src/app/app.module.ts
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  townsend-assignment-4.2
+; Author: Ethan Townsend
+; Date:   7/31/2019
+; Description: web-425
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-4/form-control/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  townsend-assignment-4.2
+; Author: Ethan Townsend
+; Date:   7/31/2019
+; Description: web-425
+;===========================================
+*/
+
+export const environment = {
+  production: true
+};
